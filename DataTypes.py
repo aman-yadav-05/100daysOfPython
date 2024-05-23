a6024e16e56619e10026ed5c45177772ebe1f226
aman <amanyadav592002@gmail.com>
2024-05-23 10:02:15 +0530

    first commit

---
+++ DataTypes.py
@@ -0,0 +1,15 @@
+#data types in python
+string="1234"
+print(type(string))
+
+number=1234
+print(type(number))
+
+float_values=1233.234
+print(type(float_values))
+
+boolean_values=True
+print(type(boolean_values))
+
+
+print(string[3])
---
+++ day1Print.py
@@ -0,0 +1,9 @@
+print("hello this is first line in the same print function \nHere starts another line from the same print function")
+
+name=input("enter your name:")
+print("your name contains " ,len(name)," characters")
+
+num1=(int)(input())
+num2=(int)(input())
+
+print(num1/num2)
